81bf2b0a0a2b33252e2ae7f2356712c6099d726b
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-07-09 11:43:51 +0700

    Chapter 3,4,5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #config/routes.rb
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/help"
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
