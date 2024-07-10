50dba46f0d97eebe245ef127a824053ea78b6050
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-07-10 10:44:16 +0700

    Chapter 7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    redirect_to root_pathend
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("welcome_msg")
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
